000cd8e527eae2ceadf1c704da865329322d679f
ScriptSculptorJS <cmcunningham27@yahoo.com>
2025-07-15 13:38:17 -0700

    created about page manually, and updated app html erb to include bootstrap

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
+  # index html view in home folder
   def index
+  end
+
+  # about html view in home folder
+  def about
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   
   # establishes which view is for the root/home when we start our server and website
   root "home#index"
+  get "home/about"
   
   get "up" => "rails/health#show", as: :rails_health_check
 
