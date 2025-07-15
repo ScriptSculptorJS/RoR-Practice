a74c8ac4406eb0201c5e9bcbf9e8e5c7b18943d8
ScriptSculptorJS <cmcunningham27@yahoo.com>
2025-07-15 13:11:43 -0700

    Created my first rails controller with view and webpage (index)

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  
+  # establishes which view is for the root/home when we start our server and website
+  root "home#index"
+  
+  get "up" => "rails/health#show", as: :rails_health_check
+
+end
